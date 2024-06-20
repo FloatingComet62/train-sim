c6be86ebea80684f449795a55adfa798a9e558da
FloatingComet62 <shubh2007rai@gmail.com>
2024-06-20 15:16:13 +0530

    customer & it's builder and related stuff

--- src/customer.rs
+++ src/customer.rs
@@ -1,7 +1,105 @@
-use crate::station::Station;
+use crate::{
+    station::{Station, Stop},
+    train::Train,
+};
 
 pub struct Customer {
     pub at: Station,
     pub goto: Station,
+    pub arrival_time: Option<u64>,
+    pub allotted_train: Option<Train>,
 }
-impl Customer {}
+
+impl Customer {
+    pub fn new(at: Station, goto: Station, arrival_time: Option<u64>) -> Self {
+        Self {
+            at,
+            goto,
+            arrival_time,
+            allotted_train: None,
+        }
+    }
+    pub fn get_allottedable_trains(&self, trains: &Vec<Train>) -> Vec<usize> {
+        let check = |upcoming_stop: &Stop,
+                     will_come_to_current_station: &mut bool,
+                     will_goto_required_station: &mut bool|
+         -> bool {
+            if upcoming_stop.station == self.at {
+                *will_come_to_current_station = true;
+            }
+            if upcoming_stop.station == self.goto {
+                *will_goto_required_station = true;
+                // the reason for returning is to discard the trains which will move in
+                // the opposite direction, aka moving from "goto" to "at"
+                return true;
+            }
+            false
+        };
+        let filter = |(i, train): (usize, &Train)| {
+            let upcoming_stops = train.upcoming_stops(self.arrival_time.unwrap_or(0));
+            let mut will_come_to_current_station = false;
+            let mut will_goto_required_station = false;
+            for upcoming_stop in upcoming_stops {
+                if check(
+                    &upcoming_stop,
+                    &mut will_come_to_current_station,
+                    &mut will_goto_required_station,
+                ) {
+                    break;
+                }
+            }
+            if will_come_to_current_station && will_goto_required_station {
+                return Some(i);
+            }
+            None
+        };
+        trains.iter().enumerate().filter_map(filter).collect()
+    }
+}
+
+pub struct CustomerBuilder {
+    pub at: Station,
+    pub goto: Option<Station>,
+    pub arrival_time: Option<u64>,
+    pub allotted_train: Option<Train>,
+}
+#[derive(Debug)]
+pub enum BuildFail {
+    MissingGotoProperty,
+}
+impl CustomerBuilder {
+    pub fn new(at: Station) -> Self {
+        Self {
+            at,
+            goto: None,
+            arrival_time: None,
+            allotted_train: None,
+        }
+    }
+    pub fn new_u32(at: u32) -> Self {
+        Self {
+            at: Station(at),
+            goto: None,
+            arrival_time: None,
+            allotted_train: None,
+        }
+    }
+    pub fn goto(mut self, goto: Station) -> Self {
+        self.goto = Some(goto);
+        self
+    }
+    pub fn goto_u32(mut self, goto: u32) -> Self {
+        self.goto = Some(Station(goto));
+        self
+    }
+    pub fn arrival_time(mut self, arrival_time: u64) -> Self {
+        self.arrival_time = Some(arrival_time);
+        self
+    }
+    pub fn build(self) -> Result<Customer, BuildFail> {
+        match self.goto {
+            None => Err(BuildFail::MissingGotoProperty),
+            Some(goto) => Ok(Customer::new(self.at, goto, self.arrival_time)),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use trains::{
-    log,
-    station::{stops_builder, Station},
-    train::{Train, TrainBuilder},
+    customer::CustomerBuilder, log, station::{stops_builder, Station}, train::{Train, TrainBuilder}
 };
 
 static MAX_ITERATIONS: u64 = 65535;
@@ -10,7 +8,7 @@ fn main() {
     let mut time: u64 = 0;
 
     let stations: Vec<Station> = (0..5).map(|n| Station(n)).collect();
-    let mut trains: Vec<Train> = vec![0, 3, 5]
+    let mut trains: Vec<Train> = vec![0]//, 3, 5]
         .iter()
         .enumerate()
         .map(|(n, &m)| {
@@ -20,17 +18,28 @@ fn main() {
         })
         .collect();
 
+    let mut customer = CustomerBuilder::new_u32(1).goto_u32(3).build().unwrap();
     loop {
         if time > MAX_ITERATIONS {
             log!(err "Fail save triggered");
             break;
         }
+        if time == 0 {
+            log!(info "Simulation Started");
+        }
         if trains.iter().all(|train| train.reached_end()) {
+            log!(info "Simulation Finished");
             break;
         }
 
+        customer.arrival_time = Some(time);
+
         trains.iter_mut().for_each(|train| train.update_state(time));
-        log!(info object trains);
+
+        println!(
+            "{:?}",
+            customer.get_allottedable_trains(&trains)
+        );
         time += 1;
     }
 }
